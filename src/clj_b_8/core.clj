de5c40a91e6892554ec057b0b3c46005c12e3287
kraulain <kraulain@gmail.com>
2023-06-20 15:09:37 +0100

    Instantiating java objects

--- src/clj_b_8/core.clj
+++ src/clj_b_8/core.clj
@@ -7,15 +7,18 @@
 
 (.addToVec v1 v2) ;; Java: v1.addtoVec(v2);
 
-
 ;; Accessing java fields
 
 (.-x point) ;;Java: point.x
-
 
 ;; Accession static methods
 
 (System/currentTimeMillis) ;;Java: System.currentTimeMillis();
 
+;; Instantiating java objects
 
+(java.util.Date.) ;; Java: new java.util.Date()
 
+(java.util.Date. (System/currentTimeMillis)) ;;Java: new java.util.Date(System/currentTimeMillis());
+
+(new java.util.Date)
